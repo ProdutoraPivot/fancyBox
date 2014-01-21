c9d4f0888eeef688eca0bb088124d2e13c08966e
Nelson Francisco <nelsondelga@gmail.com>
2014-01-21 00:00:00 -0200

    create open modal

--- demo/init-attrs/pvt.fancybox.js
+++ demo/init-attrs/pvt.fancybox.js
@@ -8,7 +8,14 @@ PVT.fancybox = {};
     'use strict';
 
     PVT.fancybox.open = function (args, undefined) {
-        
+        $.fancybox.open( 
+        	,
+        	{
+        		tpl: {
+	                closeBtn : '<a title="Fechar" class="fancybox-item fancybox-close" href="javascript:;"></a>'
+	            }
+        	} 
+		);
     };
 
 }(this, document, jQuery));
