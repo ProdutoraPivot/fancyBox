a1f3b9806ef0b3273806b6ed7b713a83686def7c
Nelson Francisco <nelsondelega@gmail.com>
2014-01-21 10:49:21 -0200

    default args in fancybox open

--- demo/init-attrs/pvt.fancybox.js
+++ demo/init-attrs/pvt.fancybox.js
@@ -7,15 +7,36 @@ PVT.fancybox = {};
 (function (window, document, $, undefined) {
     'use strict';
 
-    PVT.fancybox.open = function (args, undefined) {
-        $.fancybox.open( 
-        	,
-        	{
-        		tpl: {
-	                closeBtn : '<a title="Fechar" class="fancybox-item fancybox-close" href="javascript:;"></a>'
-	            }
-        	} 
-		);
+    PVT.fancybox.open = function (url, args, undefined) {
+        var options = {
+            width: 500,
+            height: 300
+        };
+
+        var obj = $.extend({}, options, args);
+
+        $.fancybox.open({
+            href        : url,
+            width       : obj.width,
+            height      : obj.height,
+            autoSize    : false,
+            type        : 'inline',
+            padding     : 0,
+            openEffect  : 'none',
+            closeEffect : 'none',
+            tpl: {
+                closeBtn: '<a title="Fechar" class="fancybox-close" href="#"></a>'
+            },
+            beforeShow: function () {
+
+            },
+            afterShow: function() {
+
+            },
+            afterClose: function() {
+
+            }
+        });
     };
 
 }(this, document, jQuery));
