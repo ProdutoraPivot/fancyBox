951b8cc286c8a9a6e6e7c04de9bb6797c0e17bd8
Nelson Francisco <nelsondelega@gmail.com>
2014-01-21 14:37:49 -0200

    normalize atrrs

--- demo/init-attrs/pvt.fancybox.js
+++ demo/init-attrs/pvt.fancybox.js
@@ -7,39 +7,26 @@ PVT.fancybox = {};
 (function (window, document, $, undefined) {
     'use strict';
 
-    PVT.fancybox.open = function (url, args, undefined) {
+    PVT.fancybox.open = function (url, args, callback) {
         var options = {
-            width:  500,
-            height: 300,
-            type: 'inline',
+            afterClose: function() {},
+            afterShow: function() {},
             autoSize: false,
-            padding: 0,
+            beforeShow: function () {
+                $('.fancybox-skin').css({
+                    //'border-radius': 0
+                });
+            },
+            closeClick: false,
+            closeEffect: 'fade', // fade/elastic
+            fitToView: true, // if set to true, fancyBox is resized to fit inside viewport before opening
+            height: 300,
             helpers: {
                 overlay : true,
-                locked: false
-            }
-        },
-        obj = $.extend({}, options, args);
-
-        $.fancybox.open({
-            href: url,
-            width: obj.width,
-            height: obj.height,
-            autoSize: obj.autoSize,
-            type: obj.type,
-            padding: obj.padding,
-            tpl: {
-                wrap: '<div class="fancybox-wrap" tabIndex="-1"><div class="fancybox-skin"><div class="fancybox-outer"><div class="fancybox-inner"></div></div></div></div>',
-                image: '<img class="fancybox-image" src="{href}" alt="" />',
-                error: '<p class="fancybox-error">O conteúdo solicitado não pode ser carregado.</p>',
-                closeBtn: '<a title="Fechar" class="fancybox-close" href="javascript:;"></a>',
-                next: '<a title="Próxima" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
-                prev: '<a title="Anterior" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
-            },
-            helpers: {
-                overlay : obj.helpers.overlay,
+                locked: false,
                 css: {}
             },
+            href: url,
             keys: {
                 next: {
                     13: 'left', // enter
@@ -56,22 +43,32 @@ PVT.fancybox = {};
                 close: [27], // escape key
                 play: [32], // space - start/stop slideshow
                 toggle: [70]  // letter "f" - toggle fullscreen
-            }
+            },
+            margin: 0,
+            openEffect: 'fade', // fade/elastic
+            padding: 0,
+            topRatio: 0.3,
             swf: {
                 wmode: 'transparent',
                 allowfullscreen: 'true',
                 allowscriptaccess: 'always'
             },
-            beforeShow: function () {
-
+            tpl: {
+                wrap: '<div class="fancybox-wrap" tabIndex="-1"><div class="fancybox-skin"><div class="fancybox-outer"><div class="fancybox-inner"></div></div></div></div>',
+                image: '<img class="fancybox-image" src="{href}" alt="" />',
+                error: '<p class="fancybox-error">O conteúdo solicitado não pode ser carregado.</p>',
+                closeBtn: '<a title="Fechar" class="fancybox-close" href="javascript:;"></a>',
+                next: '<a title="Próxima" class="fancybox-nav fancybox-next" href="javascript:;"><span></span></a>',
+                prev: '<a title="Anterior" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
             },
-            afterShow: function() {
+            type: 'inline',
+            width:  500,
+        },
+        obj = $.extend({}, options, args);
 
-            },
-            afterClose: function() {
+        $.fancybox.open(obj);
 
-            }
-        });
+        if (callback) callback();
     };
 
 }(this, document, jQuery));
